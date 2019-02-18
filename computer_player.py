b5044b434ec2ad1d4ae81da8172b654cc86e0425
Pusht <pushti.g.gandhi@gmail.com>
2019-02-17 20:32:03 -0500

    Computer player implementation

--- computer_player.py
+++ computer_player.py
@@ -13,6 +13,11 @@ class Computer(User):
 
 	
 	def paddle_algo(self, paddle, ball_y):
-		#need to implement
-		pass
-
+	""" Move self.paddle up or down based on the y-value of the ball.
+	The location is given by ball_y. 
+	This function returns void. 	
+	"""
+		if ball_y<self.paddle.y and self.paddle.y>self.paddle.vel:
+			self.paddle.y-=self.paddle.vel
+		elif ball_y>self.paddle.y and self.paddle.y<500-self.paddle.height-self.paddle.vel:
+			self.paddle.y+=self.paddle.vel
